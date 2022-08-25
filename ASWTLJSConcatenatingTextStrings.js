fd8305c0fef06e3a41079a367a27a21535796c39
Emanuel Joness <itzemanuelj@gmail.com>
2022-08-25 11:18:35 -0400

    final commit for string concatenation

--- ASWTLJSConcatenatingTextStrings.js
+++ ASWTLJSConcatenatingTextStrings.js
@@ -37,3 +37,45 @@ var combo = firstPart + secondPart;
 
 alert("Hi, " + userName);
 
+// Write a statement that assigns to a variable the concatenation of a string with a number. The variable has been declared beforehand.
+
+var stateWithNum = age  + 1 ;
+
+// Assign strings to two variables. Then concatenate them and assign the result to a third variable. None of the variables have been declared beforehand.
+
+var string1 = "Hello";
+var string2 = "World";
+var string3 = string1 + string2;
+alert(string3);
+
+// (The timer starts when you click in the answer field.)
+message = ("Hello," + "Dolly");
+// What is the value of message? (Include the quotation marks.)
+
+Hello,Dolly
+
+// Time goal: 30 seconds. Write a statement that concatenates two variables and assigns the result to a third variable. The third variable hasn't been declared beforehand.
+
+var vari3 = vari1 + varie2;
+
+// Time goal: 35 seconds. Write a statement that displays an alert. The message is a string concatenated with a variable.
+
+alert("Hello" + userName);
+
+// Time goal: 35 seconds. Write a statement that assigns to a variable the concatenation of a string and a number. The variable has been declared beforehand.
+
+var concStr = age = 10;
+
+// Time goal: 40 seconds. Write a statement that assigns to scribe the concatenation of the first and last names of England's greatest playwright (including the space between them). The variable hasn't been declared beforehand.
+
+var scribe = "William " + "Shakespeare";
+alert(scribe);
+
+// Time goal: 60 seconds. Assign strings to two variables. Then concatenate them and assign the result to a third variable. None of the variables have been declared beforehand.
+
+var hi = "hello";
+var there = "there";
+var  hiThere = hi = there;
+
+//watch for spaciong when writing string concenation
+
